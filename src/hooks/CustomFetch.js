021da121db842d890f74979ef2e4f1b355801362
Ianci <ianbergia.rc@gmail.com>
2020-11-05 16:36:28 -0300

    custom hook para fetchear data

--- src/hooks/CustomFetch.js
+++ src/hooks/CustomFetch.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 
-const useCustomFetch = async (value) => {
-    const key = '9EmOFaV2f4ckc3N7qTE7gOYZMx8m7bQ7'
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${encodeURI( value) }&limit=100&offset=0&rating=g&lang=en`
-    const response = await fetch(url)
-    const data = await response.json()
+const useCustomFetch = (url, time) => {
+    const [ data, setData] = useState(null)
+    const [ loading, setLoading] = useState(false)
+    const [ error, setError ] = useState(null)
     
-    const gifs = data.map(gif => {
-        return {
-            id: gif.id,
-            title: gif.title,
-            image: gif.downsized_medium.url
-
+    useEffect(() => {
+        const timeOut = setTimeout(async() => {
+            if(url){
+                setLoading(true);
+                setError(false);
+                setData(null);
+                try {
+                    const resource = await fetch(url)
+                    const dataAPI = await resource.json()
+                    setData(dataAPI.data)
+                } catch (error) {
+                    setError(error);
+                }
+                setLoading(false)
+            } else {
+                setData(null);
+                setError(null)
+            }
+            
+        }, time);
+        return () => {
+            clearTimeout(timeOut)
         }
-    })
+    }, [url, time])
+    return { data, error, loading }
 }
  
 export default useCustomFetch;
+
