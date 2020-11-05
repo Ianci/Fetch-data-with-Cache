e1600886601265ecbc3e89fab54408a4c17d27fc
Ianci <ianbergia.rc@gmail.com>
2020-11-05 16:36:42 -0300

    custom hook + usereducer para fetchear data

---
+++ src/hooks/CustomFetchReducer.js
@@ -0,0 +1,51 @@
+//Mismo custom hook pero ahora usando useReducer
+import { useEffect, useReducer } from 'react'
+
+
+const initialState = {
+    data: null,
+    error: null,
+    loading: false
+}
+const fetchReducer = (state, action) => {
+    const { type, payload } = action
+
+    switch(type){
+        case 'LOADING':
+            return{
+                ...state, loading: true, data: null, error: null
+        };
+        case 'SUCCESS':
+            return {
+                ...state, loading: false, data: payload, error: null
+            };
+        case 'FAILURE':
+            return {
+                ...state, loading: false, data: null, error: payload
+            };
+        default:
+            return state
+    };
+};
+
+export const useFetchReducer = (url, time) => {
+    const [ state, dispatch] = useReducer(fetchReducer, initialState)
+
+    useEffect(() => {
+        const timeOut = setTimeout(async() => {
+            dispatch({ type: 'LOADING'})
+            try {
+                const response = await fetch(url)
+                const dataResponse = await response.json()
+                const clearData = dataResponse.data
+                dispatch({ type: 'SUCCESS', payload: clearData})
+            } catch (error) {
+                dispatch({ type: 'FAILURE', payload: error})
+            }
+        }, time);
+        return () => {
+            clearTimeout(timeOut)
+        }
+    }, [url, time])
+        return state
+}
