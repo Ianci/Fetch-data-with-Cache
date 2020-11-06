fe8ca1fb59cabe33191821e070d2bf8d4944a232
Ianci <ianbergia.rc@gmail.com>
2020-11-06 01:21:02 -0300

    commit

--- src/components/gifForm.js
+++ src/components/gifForm.js
@@ -1,14 +1,43 @@
 import React, { useState } from 'react';
 import useCustomFetch from '../hooks/CustomFetch'
-import {useFetchReducer} from '../hooks/CustomFetchReducer'
+import { useFetchReducer } from '../hooks/CustomFetchReducer'
+import styled from '@emotion/styled'
+
+const InputStyled = styled.input`
+outline: none;
+border: 0.2px solid white;
+width: 375px;
+height: 35px;
+padding: 5px 10px;
+border-radius: 20px;
+&:hover {
+    border: 1px solid red;
+}
+`
+const ContainerInput = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const GifsGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin: 0 auto;
+    max-width: 1000px;
+`
+const GifDiv = styled.div`
+
+`
+
 const GifForm = () => {
     const [ search, setSearch ] = useState('')
     const key = '9EmOFaV2f4ckc3N7qTE7gOYZMx8m7bQ7'
     const url = search && `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${encodeURI(search)}&limit=10&offset=0&rating=g&lang=en`
     
     //Tanto useFetchReducer como useCustomFetch funcionan sin tocar ninguna otra linea de código
-    
-    const { loading, data } = useFetchReducer(url, 1250)
+
+    const { loading, data } = useFetchReducer(url, 2250)
 
     console.log(data)
 
@@ -17,20 +46,30 @@ const GifForm = () => {
     }
     return ( 
         <>
-        <input type="text" 
+        <ContainerInput>
+        <InputStyled  type="text" 
         placeholder="Search" 
         onChange={handleChange}
         value={search}
         />
-        <div className="container">
+        </ContainerInput>
+
+        <GifsGrid>
         {loading && <p>Loading...</p>}
         {data &&
         (
-            <div>
-               
-            </div>
+            <>
+                {data.map((gif)=>(
+                    <>
+                    <GifDiv>
+                    <p>{gif.title}</p>
+                    <img src={gif.images.fixed_width.url} alt={gif.title} />
+                    </GifDiv>
+                    </>
+                ))}
+            </>
         )}
-        </div>
+        </GifsGrid>
         </>
      );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import GifForm from '../components/gifForm'
+import styled from '@emotion/styled'
+import img1 from '../images/Img1.png'
+
+const Container = styled.div`
+  max-width: 1050px;
+  color: #fff;
+  margin: 0 auto;
+  min-height: 100vh;
+  @media (max-width: 992px){
+      max-width: 800px;
+      margin: 0 auto;
+      min-height: 100vh;
+  }
+`
+const Title = styled.h1`
+    display: inline-flex;
+    position: absolute;
+    top: 25px;
+    padding-left: 220px;
+    font-size: 35px;
+`
+
 const Home = () => {
     return ( 
         <>
-        <h1>Bienvenido a Giphy! Busca los mejores gifs aquí</h1>
+        <Container>
+        <img src={img1} alt="img-logo" style={{width: "140px"}}/>
+        <Title>Bienvenido a Gifhy! </Title>
+        
+
         <GifForm />
+
+        </Container>
         </>
      );
 }
